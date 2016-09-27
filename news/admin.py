470c30e4b9f9790149b38bb9e59fc2aaffbbf82f
vovan <vovan>
2016-09-27 13:51:07 +0700

    added categories

--- news/admin.py
+++ news/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Article
 from .models import Comment
+from .models import Category
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
 	exclude = ('slug', 'author')
 
 admin.site.register(Comment)
+admin.site.register(Category)
+
