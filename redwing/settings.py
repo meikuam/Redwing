aad83b2fc8a3031e8a2f8477e48a091b44664980
vovan <vovan>
2016-09-27 14:41:39 +0700

    base.html

--- redwing/settings.py
+++ redwing/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'redwing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'redwing/templates'), os.path.join(BASE_DIR, 'news/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
