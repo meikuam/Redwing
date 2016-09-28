710c65fd5c9f6c09936c684e69475046ece7d6c6
features94 <pspsfun@gmail.com>
2016-09-28 22:51:06 +0700

    fix article creation in admin

--- news/admin.py
+++ news/admin.py
@@ -8,6 +8,11 @@ from .models import Category
 class ArticleAdmin(admin.ModelAdmin):
 	exclude = ('slug', 'author')
 
+	def save_model(self, request, obj, form, change):
+		if getattr(obj, 'author', None) is None:
+			obj.author = request.user
+		obj.save()
+
 admin.site.register(Comment)
 admin.site.register(Category)
 
