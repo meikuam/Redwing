3c1a3bd32c1afe1bb6a03cdf1e5fc60fd6a113d5
features94 <pspsfun@gmail.com>
2016-09-29 15:10:15 +0700

    comment box markup + styling

--- news/forms.py
+++ news/forms.py
@@ -4,4 +4,8 @@ from ckeditor.widgets import CKEditorWidget
 from .models import Article
 
 class CommentForm(forms.Form):
-	text = forms.CharField(label='Comment', max_length=200)
+	text = forms.CharField(
+		label='Comment',
+		max_length=200,
+		widget=forms.Textarea(attrs={'rows': 4, 'cols': 40, 'placeholder': 'Write a comment...'})
+	)
