123b3bced9fd4c9045fc4a98a515c4f8596cd6dd
vovan <m8r-uiq5041@mailinator.com>
2016-09-30 21:57:09 +0700

    admin interface changes

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 
-from .models import Article, Comment, Category, ContentManagerCategory
+#from .models import Article
+from .models import Comment, Category, ContentManagerCategory
 
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-	exclude = ('slug', 'author')
+#@admin.register(Article)
+#class ArticleAdmin(admin.ModelAdmin):
+#	exclude = ('slug', 'author')
+#
+#	def save_model(self, request, obj, form, change):
+#		if getattr(obj, 'author', None) is None:
+#			obj.author = request.user
+#		obj.save()
 
-	def save_model(self, request, obj, form, change):
-		if getattr(obj, 'author', None) is None:
-			obj.author = request.user
-		obj.save()
-
-admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(ContentManagerCategory)
 
