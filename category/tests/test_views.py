6e5faf28d47741d497055a10c81d16df0dd8b4fc
wowshakhov <m8r-uiq5041@mailinator.com>
2016-12-04 14:41:56 +0700

    Update test_views.py

--- category/tests/test_views.py
+++ category/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import Client
 from category.models import Category
 
 
-class CaegoriesTestCase(TestCase):
+class CategoriesTestCase(TestCase):
     def test_categories(self):
         c = Client()
         cat = Category.objects.create(name="test_category")
