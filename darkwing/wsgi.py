bcdb0d2aa8aa4a6231d3557b25d72007ebac014c
meikuam <meikuam@gmail.com>
2017-10-24 19:02:36 +0000

    add branch darkwing

--- darkwing/wsgi.py
+++ darkwing/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for redwing project.
+WSGI config for darkwing project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "darkwing.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redwing.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redwing.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "darkwing.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
